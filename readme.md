09cc442dca480bf6123ce28b668735e0cde352b6
Arley Yamid Taborda <arleytaborda93@hotmail.com>
2023-01-24 10:25:20 -0500

    Se agrega eliminación de acentos, documentación JS

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Proyecto encriptador de texto 🔏 Alura Latam
+
+___
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+___
+Proyecto inicial del programa Alura Latam + ONE Oracle Next Education, consistente en crear un encriptador de texto con las siguientes llaves:
+
+~~~text
+La letra "a" es convertida en "ai"
+La letra "e" es convertida en "enter"
+La letra "i" es convertida en "limes"
+La letra "o" es convertida en "ober"
+La letra "u" es convertida en "ufat"
+~~~
+
+## ✔️ Requisitos del desafío
+
+* _Debe funcionar solo con letras minúsculas_
+* _No deben ser utilizados letras con acentos ni caracteres especiales_
+* _Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original._
+
+### ❕Algunas consideraciones
+
+* _Se modifica para que si bien reciba letras en minúsculas o mayúsculas, todas sean convertidas en letras minúsculas, por lo que el texto recibido puede estar escrito en letras mayúsculas pero el sistema lo transforma en letras minúsculas_
+
+* _Se pueden recibir acentos, pero estos son eliminados, de esta forma al desencriptar el texto el resultado mostrado no sería el original por lo cual se recomienda solo usar letras minúsculas y sin acento: "Se recomienda solo letras minúsculas y sin acento"_
+
+___
+[Encriptador](https://arleytaborda.github.io/Challenge-1/)
+___
+
+[![LinkedIn](img/LinkedIn-Blue.png)](https://www.linkedin.com/in/arleyyamidtaborda/)
+[![GitHub](img/GitHub.png)](https://github.com/ArleyTaborda)
+
+Arley Yamid Taborda Taborda
--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-function encriptar(){
-    var texto = document.getElementById("text-input").value.toLowerCase();
-    texto = texto.toLowerCase();
-    var txt_cifrado = texto.replace(/e/igm,"enter").replace(/o/igm,"ober").replace(/i/igm,"imes").replace(/a/igm,"ai").replace(/u/igm,"ufat");
-
+//Se define la función para encriptar el texto
+function encriptar() {
+  //Se define la variable texto que recibe el texto que el usuario escriba y lo convierte todo en letras minúsculas
+  var texto = document.getElementById("text-input").value.toLowerCase();
+  //Se valida que el usuario haya ingresado un texto, caso contrario se arroja una alerta para obligar a que se especifique algún texto a encriptar
+  if (texto.length > 0) {
+    //Se define la variable txtCifrado en el cual se almacena el nuevo texto cifrado
+    var txtCifrado = texto.replace(/[eéëèê]/igm, "enter").replace(/[oóòôö]/igm, "ober").replace(/[iìíîï]/igm, "imes").replace(/[aàâáä]/igm, "ai").replace(/[uùúûú]/igm, "ufat");
+    //Se modifica el estilo visual del panel derecho, para mostrar el texto cifrado
     document.getElementById("right").style.backgroundImage = "none";
     document.getElementById("texto1").style.display = "none";
     document.getElementById("copiar").style.display = "show";
-    document.getElementById("texto2").innerHTML = txt_cifrado;
+    document.getElementById("texto2").innerHTML = txtCifrado;
     document.getElementById("text-input").innerHTML = " ";
     document.getElementById("copiar").style.display = "inherit";
-
   }
-
-  function desencriptar() {
-    var texto = document.getElementById("text-input").value.toLowerCase();
-    var txt_cifrado = texto.replace(/enter/igm,"e").replace(/ober/igm,"o").replace(/imes/igm,"i").replace(/ai/igm,"a").replace(/ufat/igm,"u");
-
+  else {
+    alert("Por favor ingresa algún texto para encriptar.")
+  }
+}
+//Se define la función para desencriptar el texto
+function desencriptar() {
+  //Se define la variable texto que recibe el texto que el usuario escriba y lo convierte todo en letras minúsculas
+  var texto = document.getElementById("text-input").value.toLowerCase();
+  if (texto.length > 0) {
+    var txtCifrado = texto.replace(/enter/igm, "e").replace(/ober/igm, "o").replace(/imes/igm, "i").replace(/ai/igm, "a").replace(/ufat/igm, "u");
     document.getElementById("right").style.backgroundImage = "none";
     document.getElementById("texto1").style.display = "none";
     document.getElementById("copiar").style.display = "show";
-    document.getElementById("texto2").innerHTML = txt_cifrado;
+    document.getElementById("texto2").innerHTML = txtCifrado;
     document.getElementById("text-input").innerHTML = " ";
     document.getElementById("copiar").style.display = "inherit";
   }
-
-  function copiar() {
-    var encrypted_text = document.getElementById("texto2");
-    navigator.clipboard.writeText(encrypted_text.value).then(()=> {console.log("Texto copiado exitosamente"); alert("Texto copiado exitosamente");}).catch(err => {console.error("Error: ", err); alert("Error: ") + err});
+  else {
+    alert("Por favor ingresa algún texto para desencriptar.")
   }
+}
+//Se define la función para copiar el texto resultado
+function copiar() {
+  var encrypted_text = document.getElementById("texto2");
+  navigator.clipboard.writeText(encrypted_text.value).then(() => { console.log("Texto copiado exitosamente"); alert("Texto copiado exitosamente"); }).catch(err => { console.error("Error: ", err); alert("Error: ") + err });
+}
